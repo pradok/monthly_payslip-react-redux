2bdb6124ed82b7af9072a3232d0531478fa90f7c
Pradosh Sinha <pradoshks@outlook.com>
2017-03-13 23:48:01 +1100

    Modified Pay Slip to allow empty props and show relevant message.

--- src/modules/employeePaySlip/components/EmployeePaymentSlipComponent.js
+++ src/modules/employeePaySlip/components/EmployeePaymentSlipComponent.js
@@ -2,42 +2,53 @@ import React, {PropTypes} from 'react'
 
 
 const EmployeePaymentSlipComponent = (props) => {
+    const propsEmpty = Object.keys(props).length;
     const {payPeriod, employeeName, grossIncome, incomeTax, netIncome, superAnnuation} = props;
 
-    return (
-        <div>
-            <legend>Payslip</legend>
+    if (props.constructor === Object && propsEmpty == 0) {
+        return (
             <div>
-                <label>Pay Slip for the month: </label> {payPeriod}
+                <legend>Pay Slip</legend>
+                <p>Unable to print Pay Slip. No Employee details provided</p>
             </div>
+        )
+    }
+    else {
+        return (
             <div>
-                <label>Employee Name:</label> {employeeName}
+                <legend>Payslip</legend>
+                <div>
+                    <label>Pay Slip for the month: </label> {payPeriod}
+                </div>
+                <div>
+                    <label>Employee Name:</label> {employeeName}
+                </div>
+                <div>
+                    <label>Gross Income:</label> {grossIncome}
+                </div>
+                <div>
+                    <label>Income Tax:</label> {incomeTax}
+                </div>
+                <div>
+                    <label>Net Income:</label> {netIncome}
+                </div>
+                <div>
+                    <label>Super:</label> {superAnnuation}
+                </div>
             </div>
-            <div>
-                <label>Gross Income:</label> {grossIncome}
-            </div>
-            <div>
-                <label>Income Tax:</label> {incomeTax}
-            </div>
-            <div>
-                <label>Net Income:</label> {netIncome}
-            </div>
-            <div>
-                <label>Super:</label> {superAnnuation}
-            </div>
-        </div>
-    )
+        )
+    }
 
 }
 
 
 EmployeePaymentSlipComponent.propTypes = {
-    payPeriod: PropTypes.string.isRequired,
-    employeeName: PropTypes.string.isRequired,
-    grossIncome: PropTypes.number.isRequired,
-    incomeTax: PropTypes.number.isRequired,
-    netIncome: PropTypes.number.isRequired,
-    superAnnuation: PropTypes.number.isRequired,
+    payPeriod: PropTypes.string,
+    employeeName: PropTypes.string,
+    grossIncome: PropTypes.number,
+    incomeTax: PropTypes.number,
+    netIncome: PropTypes.number,
+    superAnnuation: PropTypes.number,
 }
 
 
